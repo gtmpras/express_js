7cebd057093d417a6d5088073927ad4fce472af7
Prasoon Gautam <109397711+gtmpras@users.noreply.github.com>
2025-06-17 12:54:41 +0545

    day 1

---
+++ src/index.mjs
@@ -0,0 +1,57 @@
+
+//*****Basics of Express JS */
+
+// import express from 'express';
+
+// const app = express();
+
+// const PORT = process.env.PORT || 3000;
+
+// app.listen(PORT,()=> {
+//     console.log(`Running on Port ${PORT}`);
+// });
+
+
+//**Let's Dive to level 2 */
+
+import express from 'express';
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+const mockUsers = [
+    { id:1, userName:'prasoon', displayName:"Prasoon"},
+    { id:2, userName:'prakritee', displayName: 'Prakritee'}
+ ]
+
+//adding GET method here To continue GET REQUEST
+app.get("/", (request, response)=> {
+    response.status(201).send({msg: "Hello, Prasoon"});
+});
+
+
+app.get("/api/users", (request, response)=>{
+    response.send(mockUsers);
+});
+
+app.get("/api/products",(request, response)=>{
+    response.send([{
+        id:123, name:"Coffee", price: 200
+    }]);
+});
+
+//using route parameters(Single User record)
+app.get("/api/users/:id",(request,response)=>{
+    console.log(request.params);
+    const parsedId = parseInt(request.params.id);
+    console.log(parsedId);
+        if(isNaN(parsedId)) return response.status(400).send({msg:"Bad Request. Invalid ID"});
+
+    const findUser = mockUsers.find((user)=> user.id === parsedId);
+    if(!findUser) return response.sendStatus(404);
+    return response.send(findUser);
+    });
+
+app.listen(PORT,()=> {
+    console.log(`Running on Port ${PORT}`);
+});
