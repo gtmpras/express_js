bc5f122ff24727cb042238bb381b7f7231f6228f
Prasoon Gautam <109397711+gtmpras@users.noreply.github.com>
2025-06-26 00:28:00 +0545

    multi_user session handling

--- src/index.mjs
+++ src/index.mjs
@@ -129,3 +129,22 @@ app.get('/api/auth/status', (request, response)=>{
     return request.session.user ? response.status(200).send(request.session.user):
     response.status(401).send({msg: "Not Authenticated"});
 });
+
+app.post('/api/cart',(request, response)=>{
+    if(!request.session.user) return response.sendStatus(401);
+    const {body: item} = request;
+
+    const { cart}= request.session;
+
+    if(cart){
+        cart.push(item);
+    }else{
+        request.session.cart = [item];
+    }
+    return response.status(201).send(item);
+});
+
+app.get('/api/cart', (request, response)=> {
+    if(!request.session.user )return response.sendStatus(401);
+    return response.send(request.session.cart ?? []);
+});
